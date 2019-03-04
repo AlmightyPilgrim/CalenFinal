a1b35c8ef8caf75d20b9af579f54b7f4b64107be
Bret Hayes <bret_hayes@comcast.net>
2019-03-04 07:37:37 -0500

    Add project files.

---
+++ FinaleCalendar/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinaleCalendar
+{
+    public class Class1
+    {
+    }
+}
